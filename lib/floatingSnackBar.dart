ee1b7a06efec9ade246302ac3352383470a402fd
Muhammed Ameen <59697692+muhd-ameen@users.noreply.github.com>
2022-12-14 16:19:19 +0530

    plugin add

---
+++ lib/floatingSnackBar.dart
@@ -0,0 +1,3 @@
+library colorful_progress_indicators;
+
+export 'package:FloatingSnackBar/src/floatingSnackBar.dart';
---
+++ lib/src/floatingSnackBar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void FloatingSnackBar({
+  required String message,
+  required BuildContext context,
+  Duration? duration,
+  TextStyle? textStyle,
+  Color? textColor,
+  Color? backgroundColor,
+}) {
+  var snack = SnackBar(
+    behavior: SnackBarBehavior.floating,
+    margin: const EdgeInsets.all(20),
+    duration: duration ?? const Duration(milliseconds: 4000),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+    content: Text(
+      message,
+      textScaleFactor: 1,
+      style: textStyle ?? TextStyle(color: textColor ?? Colors.white),
+    ),
+    backgroundColor: backgroundColor ?? Colors.black.withAlpha(200),
+  );
+  ScaffoldMessenger.of(context).showSnackBar(snack);
+}
