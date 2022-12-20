e29db3bc70580b1c329c57ed92c8a25d91596ec8
Muhammed Ameen <ameens.in19@gmail.com>
2022-12-21 00:25:21 +0530

    Project Rename

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:nod_flur/floatingSnackBar.dart';
+import 'package:floating_snackbar/floatingSnackBar.dart';
 
 void main() {
   runApp(const MyApp());
