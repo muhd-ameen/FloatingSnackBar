d29f2dc98a74b9543d7ccec96e68b8d1cfdc4b27
Muhammed Ameen <ameens.in19@gmail.com>
2022-12-21 01:40:52 +0530

    README UPDATE

--- lib/floatingSnackBar.dart
+++
@@ -1,4 +0,0 @@
-// ignore: file_names
-library colorful_progress_indicators;
-
-export 'package:floating_snackbar/src/floatingSnackBar.dart';
---
+++ lib/floating_snackbar.dart
@@ -0,0 +1,4 @@
+// ignore: file_names
+library floatingSnackBar;
+
+export 'package:floating_snackbar/src/floatingSnackBar.dart';
